eab8aa87f71b446ce2e2d910a99d9e58e230e332
herpydirtz <georgechan930@gmail.com>
2023-02-16 23:25:57 -0500

    finalized working weather module

--- apps/weather.py
+++ apps/weather.py
@@ -36,13 +36,22 @@ def pas(output):  #print and speak function
     print(output)
     speak(output)
 
-def weather():
-    locator = parse_request()
-    with open("email.txt", "r") as f:
-        email = f.read()
-    geolocator = Nominatim(user_agent=email)
-    location = geolocator.geocode(locator)  #initialize locator and find the location
-    
+def weather(request):
+    weather_request = request
+    if "in" in weather_request:
+        x = weather_request.index("in")
+        while int(x) != -1:
+            del(weather_request[0])
+            x -= 1
+        with open("email.txt", "r") as f:
+            email = f.read()
+        geolocator = Nominatim(user_agent=email)
+        location = geolocator.geocode(weather_request)  #initialize locator and find the location
+    else:
+        with open("email.txt", "r") as f:
+            email = f.read()
+        geolocator = Nominatim(user_agent=email)
+        location = geolocator.geocode("New York")
     BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
     with open("api_key.txt", "r") as f:
         API_KEY = f.read()
@@ -68,7 +77,8 @@ def weather():
         pas(response['weather'])
 
 
-weather()
+
+
 
 #{'coord': {'lon': 74.006, 'lat': 40.7128}, 'weather': [{'id': 801, 'main': 'Clouds', 'description': 'few clouds', 'icon': '02n'}], 'base': 'stations',
 # 'main': {'temp': 235.45, 'feels_like':9}, 'clouds': {'all': 11}, 'dt': 1673554929, 'sys': {'country': 'KG', 'sunrise': 1673576805, 'sunset': 1673611061}, 
@@ -77,6 +87,6 @@ weather()
 # 40.7128, -74.0060
 
 #def weather():
-
+#have a default location the weather pulls from 
 #whats the weather like in XYZ 
 #loop through each word after in and concatenate it in a new variable to be input into the geopy lib
